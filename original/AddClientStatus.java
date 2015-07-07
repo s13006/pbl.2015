a100b77c2d933dac1a7e851d5633068c8eeea1bb
s13006 <s13006@std.it-college.ac.jp>
2015-07-07 16:10:59 +0900

    2015/07/07 update

--- original/AddClientStatus.java
+++ original/AddClientStatus.java
@@ -5,67 +5,67 @@
  */
 public class AddClientStatus extends ConsoleStatus {
 
-	// フィールド
-	private ClientList cl;
+    // フィールド
+    private ClientList cl;
 
-	private String[] messages = {
+    private String[] messages = {
             "会社名を入力してください。>",
             "住所を入力してください。>",
             "電話番号を入力してください。>"
-	};
-	private String[] data = new String[ 6 ];
+    };
+    private String[] data = new String[ 6 ];
 
-	/**
-	 * コンストラクタ AddPersonStatus
-	 * @param String firstMess
-	 * @param String promptMess
-	 * @param boolean IsEndStatus
-	 * @param PersonList cl
-	 */
-	AddClientStatus(String firstMess, String promptMess,
-					boolean IsEndStatus, ClientList cl) {
-		super( firstMess, promptMess, IsEndStatus );
-		this.cl = cl;
-	}
+    /**
+     * コンストラクタ AddPersonStatus
+     * @param String firstMess
+     * @param String promptMess
+     * @param boolean IsEndStatus
+     * @param PersonList cl
+     */
+    AddClientStatus(String firstMess, String promptMess,
+                    boolean IsEndStatus, ClientList cl) {
+        super( firstMess, promptMess, IsEndStatus );
+        this.cl = cl;
+    }
 
-	// 最初に出力するメッセージを表示する
-	// このクラスでは従業員のデータの入力処理
-	// のみを行う
-	/** displayFirstMess
-	 * @throws Exception
-	 */
-	public void displayFirstMess() throws Exception {
-		// messagesの各文字列を順に表示しながら
-		// キーボードから氏名，住所などを得ていく
-		for( int idx = 0; idx < messages.length; idx++ ) {
-			System.out.print( messages[ idx ] );
-			data[ idx ] = inputMessage();
-		}
+    // 最初に出力するメッセージを表示する
+    // このクラスでは従業員のデータの入力処理
+    // のみを行う
+    /** displayFirstMess
+     * @throws Exception
+     */
+    public void displayFirstMess() throws Exception {
+        // messagesの各文字列を順に表示しながら
+        // キーボードから氏名，住所などを得ていく
+        for( int idx = 0; idx < messages.length; idx++ ) {
+            System.out.print( messages[ idx ] );
+            data[ idx ] = inputMessage();
+        }
 
-		try {
+        try {
             String record = cl.size() + "," + data[ 0 ] + "," + data[ 1 ] + "," + data[ 2 ] + ",f";
 
             Client new_c = new Client(record);
 
-			// 新しいレコードを追加
-			cl.add(record);
-			System.out.println( "ID:" + new_c.getID() + "で登録されました。" );
-		} catch( NumberFormatException e ) {
-			System.out.println( "数値に変換できないデータが入力されています。" );
-			System.out.println( "再入力してください。" );
-			displayFirstMess();
-			return;
-		}
-	}
+            // 新しいレコードを追加
+            cl.add(record);
+            System.out.println( "ID:" + new_c.getID() + "で登録されました。" );
+        } catch( NumberFormatException e ) {
+            System.out.println( "数値に変換できないデータが入力されています。" );
+            System.out.println( "再入力してください。" );
+            displayFirstMess();
+            return;
+        }
+    }
 
-	// 次の状態に遷移することを促すためのメッセージの表示
-	// このクラスは，初期状態に戻ると決まっているため，何が
-	// 入力されても初期状態に遷移するようにしている
-	/** getNextStatus
-	 * @param String s
-	 * @return ConsoleStatus
-	 */
-	public ConsoleStatus getNextStatus( String s ) {
-		return super.getNextStatus( " " );
-	}
+    // 次の状態に遷移することを促すためのメッセージの表示
+    // このクラスは，初期状態に戻ると決まっているため，何が
+    // 入力されても初期状態に遷移するようにしている
+    /** getNextStatus
+     * @param String s
+     * @return ConsoleStatus
+     */
+    public ConsoleStatus getNextStatus( String s ) {
+        return super.getNextStatus( " " );
+    }
 }
